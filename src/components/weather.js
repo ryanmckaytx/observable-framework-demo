40f5846db44b7906f74a2686eb42a41ce5193310
Ryan McKay <ryan.michael.mckay@gmail.com>
2024-08-04 15:59:33 -0500

    add precip chart

--- src/components/weather.js
+++ src/components/weather.js
@@ -18,3 +18,22 @@ export function temperaturePlot(forecast, {width, height} = {}) {
     ]
   });
 }
+
+export function precipPlot(forecast, {width, height} = {}) {
+  return Plot.plot({
+    title: "Hourly precipitation % forecast",
+    width,
+    height,
+    x: {type: "utc", ticks: "day", label: null},
+    y: {grid: true, inset: 10, label: "Precip %"},
+    marks: [
+      Plot.lineY(forecast.properties.periods, {
+        x: "startTime",
+        y: (d) => d.probabilityOfPrecipitation.value,
+        z: null, // varying color, not series
+        // stroke: "probabilityOfPrecipitation",
+        curve: "step-after"
+      })
+    ]
+  });
+}
--- src/weather.md
+++ src/weather.md
@@ -13,9 +13,11 @@ display(forecast)
 ```
 
 ```js
-import {temperaturePlot} from "./components/temperaturePlot.js";
+import {temperaturePlot} from "./components/weather.js";
+import {precipPlot} from "./components/weather.js";
 ```
 
 <div class="grid grid-cols-1">
   <div class="card">${resize((width) => temperaturePlot(forecast, {width}))}</div>
+  <div class="card">${resize((width) => precipPlot(forecast, {width}))}</div>
 </div>
